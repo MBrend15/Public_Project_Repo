a07fd01aa906391e7fe4a048475f4d48c958c723
michaelmalave-ucb <michaelmalave@berkeley.edu>
2023-02-07 16:54:01 -0800

    adding resources for s3 resources

--- kms.tf
+++ kms.tf
@@ -1,11 +1,11 @@
 # Encryption
 resource "aws_kms_key" "sapient-bucket-key" {
-  description             = "This key is used to encrypt bucket objects"
+  description             = "This key is used to encrypt sapient bucket objects"
   deletion_window_in_days = 10
   policy                  = data.aws_iam_policy_document.kms-key-policy-doc.json
 }
 
-resource "aws_kms_alias" "analytics-bucket-key-alias" {
-  name          = "alias/analytics-bucket-key"
+resource "aws_kms_alias" "sapient-bucket-key-alias" {
+  name          = "alias/sapient-bucket-key"
   target_key_id = aws_kms_key.sapient-bucket-key.key_id
 }
---
+++ s3.tf
@@ -0,0 +1,72 @@
+locals {
+  bucket_prefix = "sapient-bucket"
+  bucket_stages = {
+    "raw"     = { environment = "Prod" }
+    "trusted" = { environment = "Prod" }
+    "refined" = { environment = "Prod" }
+  }
+}
+
+
+resource "aws_s3_bucket" "sapient-buckets" {
+  for_each = local.bucket_stages
+
+  bucket = "${local.bucket_prefix}-${each.key}"
+
+  tags = {
+    Name = local.bucket_prefix
+  }
+}
+
+resource "aws_s3_bucket_acl" "sapient-bucket-acl" {
+  for_each = local.bucket_stages
+
+  bucket = aws_s3_bucket.sapient-buckets[each.key].id
+  acl    = "private"
+}
+
+#block public access
+resource "aws_s3_bucket_public_access_block" "aws-public-access-config" {
+  for_each = local.bucket_stages
+
+  bucket = aws_s3_bucket.sapient-buckets[each.key].id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+
+resource "aws_s3_bucket_cors_configuration" "aws-cors-config" {
+  for_each = local.bucket_stages
+
+  bucket = aws_s3_bucket.sapient-buckets[each.key].bucket
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+
+  cors_rule {
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+  }
+}
+
+# Encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "aws-s3-bucket-config" {
+  for_each = local.bucket_stages
+
+  bucket = aws_s3_bucket.sapient-buckets[each.key].bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.sapient-bucket-key.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
